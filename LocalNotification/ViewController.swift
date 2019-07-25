af0726648075bcb6454527bf5906b5ba28cd33c0
Michiru <starffhwxx@gmil.com>
2019-07-25 11:50:42 +0800

    no message

--- LocalNotification/ViewController.swift
+++ LocalNotification/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldForTitle: UITextField!
+    
+    @IBOutlet weak var textFieldFoeContent: UITextField!
+    
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    
+    
+    
     }
 
 
